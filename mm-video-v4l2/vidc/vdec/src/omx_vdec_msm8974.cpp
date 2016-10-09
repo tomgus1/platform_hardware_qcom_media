CyanogenMod/android_hardware_qcom_media

commit 54bfdc2e227da27c59b26cc6414aecb719328114

mm-video-v4l2: vidc: vdec: Add support for S3D metadata
Stereo video output information is present in H264 SEI. Venus HW
decodes it and sends to OMX component as extradata. OMX component
parses extradata and sends this information to display as metadata.

Change-Id: I08af0a408e4c44c396587600a7ea1b5a61fbe5a9

mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
@@ -128,7 +128,7 @@ extern "C" {
 #define EXTRADATA_IDX(__num_planes) (__num_planes  - 1)
 #define ALIGN(x, to_align) ((((unsigned) x) + (to_align - 1)) & ~(to_align - 1))
 
-#define DEFAULT_EXTRADATA (OMX_INTERLACE_EXTRADATA)
+#define DEFAULT_EXTRADATA (OMX_INTERLACE_EXTRADATA | OMX_FRAMEPACK_EXTRADATA)
 #define DEFAULT_CONCEAL_COLOR "32784" //0x8010, black by default
 
 
@@ -591,6 +591,7 @@ omx_vdec::omx_vdec(): m_error_propogated(false),
     m_other_extradata(NULL),
     m_profile(0),
     client_set_fps(false),
+    stereo_output_mode(HAL_NO_3D),
     m_last_rendered_TS(-1),
     m_queued_codec_config_count(0),
     secure_scaling_to_non_secure_opb(false)
@@ -7303,6 +7304,13 @@ OMX_ERRORTYPE omx_vdec::fill_buffer_done(OMX_HANDLETYPE hComp,
                          UPDATE_REFRESH_RATE, (void*)&current_framerate);
         }
 
+        if (buffer->nFilledLen && m_enable_android_native_buffers && m_out_mem_ptr) {
+            OMX_U32 buf_index = buffer - m_out_mem_ptr;
+            DEBUG_PRINT_LOW("stereo_output_mode = %d",stereo_output_mode);
+            setMetaData((private_handle_t *)native_buffer[buf_index].privatehandle,
+                               S3D_FORMAT, (void*)&stereo_output_mode);
+        }
+
         if (il_buffer) {
             log_output_buffers(il_buffer);
             if (dynamic_buf_mode) {
@@ -9267,6 +9275,26 @@ void omx_vdec::handle_extradata(OMX_BUFFERHEADERTYPE *p_buf_hdr)
                 case MSM_VIDC_EXTRADATA_S3D_FRAME_PACKING:
                     struct msm_vidc_s3d_frame_packing_payload *s3d_frame_packing_payload;
                     s3d_frame_packing_payload = (struct msm_vidc_s3d_frame_packing_payload *)(void *)data->data;
+                    switch (s3d_frame_packing_payload->fpa_type) {
+                        case MSM_VIDC_FRAMEPACK_SIDE_BY_SIDE:
+                            if (s3d_frame_packing_payload->content_interprtation_type == 1)
+                                stereo_output_mode = HAL_3D_SIDE_BY_SIDE_L_R;
+                            else if (s3d_frame_packing_payload->content_interprtation_type == 2)
+                                stereo_output_mode = HAL_3D_SIDE_BY_SIDE_R_L;
+                            else {
+                                DEBUG_PRINT_ERROR("Unsupported side-by-side framepacking type");
+                                stereo_output_mode = HAL_NO_3D;
+                            }
+                            break;
+                        case MSM_VIDC_FRAMEPACK_TOP_BOTTOM:
+                            stereo_output_mode = HAL_3D_TOP_BOTTOM;
+                            break;
+                        default:
+                            DEBUG_PRINT_ERROR("Unsupported framepacking type");
+                            stereo_output_mode = HAL_NO_3D;
+                    }
+                    DEBUG_PRINT_LOW("setMetaData FRAMEPACKING : fpa_type = %lu, content_interprtation_type = %lu, stereo_output_mode= %d",
+                        s3d_frame_packing_payload->fpa_type, s3d_frame_packing_payload->content_interprtation_type, stereo_output_mode);
                     if (client_extradata & OMX_FRAMEPACK_EXTRADATA) {
                         append_framepack_extradata(p_extra, s3d_frame_packing_payload);
                         p_extra = (OMX_OTHER_EXTRADATATYPE *) (((OMX_U8 *) p_extra) + p_extra->nSize);
@@ -9525,7 +9553,7 @@ void omx_vdec::print_debug_extradata(OMX_OTHER_EXTRADATATYPE *extra)
                 "                  cancel_flag: %u \n"
                 "                         type: %u \n"
                 " quincunx_sampling_flagFormat: %u \n"
-                "  content_interpretation_type: %u \n"
+                "  content_interprtation_type: %u \n"
                 "        spatial_flipping_flag: %u \n"
                 "          frame0_flipped_flag: %u \n"
                 "             field_views_flag: %u \n"

mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
@@ -969,6 +969,7 @@ class omx_vdec: public qc_omx_component
 
         unsigned int m_fill_output_msg;
         bool client_set_fps;
+        unsigned int stereo_output_mode;
         class allocate_color_convert_buf
         {
             public:

tomgus1/platform_hardware_qcom_media

commit 253c114c80736a3e80c74f1f78a9dee6212b345d

media: fix typos
introduced in commit: 54bfdc2e227da27c59b26cc6414aecb719328114
correct commit: https://source.codeaurora.org/quic/la/platform/hardware/qcom/media/diff/?h=LA.BR.1.3.4_rb1.22&id=ca77ffb98cd5b952bdceea391e5452ea9614ebad

Signed-off-by: David Viteri <davidteri91@gmail.com>

mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
@@ -9273,9 +9273,9 @@ void omx_vdec::handle_extradata(OMX_BUFFERHEADERTYPE *p_buf_hdr)
                     s3d_frame_packing_payload = (struct msm_vidc_s3d_frame_packing_payload *)(void *)data->data;
                     switch (s3d_frame_packing_payload->fpa_type) {
                         case MSM_VIDC_FRAMEPACK_SIDE_BY_SIDE:
-                            if (s3d_frame_packing_payload->content_interprtation_type == 1)
+                            if (s3d_frame_packing_payload->content_interpretation_type == 1)
                                 stereo_output_mode = HAL_3D_SIDE_BY_SIDE_L_R;
-                            else if (s3d_frame_packing_payload->content_interprtation_type == 2)
+                            else if (s3d_frame_packing_payload->content_interpretation_type == 2)
                                 stereo_output_mode = HAL_3D_SIDE_BY_SIDE_R_L;
                             else {
                                 DEBUG_PRINT_ERROR("Unsupported side-by-side framepacking type");
@@ -9289,8 +9289,8 @@ void omx_vdec::handle_extradata(OMX_BUFFERHEADERTYPE *p_buf_hdr)
                             DEBUG_PRINT_ERROR("Unsupported framepacking type");
                             stereo_output_mode = HAL_NO_3D;
                     }
-                    DEBUG_PRINT_LOW("setMetaData FRAMEPACKING : fpa_type = %d, content_interprtation_type = %d, stereo_output_mode= %d",
-                        s3d_frame_packing_payload->fpa_type, s3d_frame_packing_payload->content_interprtation_type, stereo_output_mode);
+                    DEBUG_PRINT_LOW("setMetaData FRAMEPACKING : fpa_type = %d, content_interpretation_type = %d, stereo_output_mode= %d",
+                        s3d_frame_packing_payload->fpa_type, s3d_frame_packing_payload->content_interpretation_type, stereo_output_mode);
                     if (client_extradata & OMX_FRAMEPACK_EXTRADATA) {
                         append_framepack_extradata(p_extra, s3d_frame_packing_payload);
                         p_extra = (OMX_OTHER_EXTRADATATYPE *) (((OMX_U8 *) p_extra) + p_extra->nSize);
@@ -9553,7 +9553,7 @@ void omx_vdec::print_debug_extradata(OMX_OTHER_EXTRADATATYPE *extra)
                 "                  cancel_flag: %u \n"
                 "                         type: %u \n"
                 " quincunx_sampling_flagFormat: %u \n"
-                "  content_interprtation_type: %u \n"
+                "  content_interpretation_type: %u \n"
                 "        spatial_flipping_flag: %u \n"
                 "          frame0_flipped_flag: %u \n"
                 "             field_views_flag: %u \n"